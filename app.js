6694da1c5d07db1b72187d48f1a5ebdbd5253ec0
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-19 11:04:04 -0400

    added menu

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
+import Store from "./services/Store";
+import API from "./services/API";
+import { loadData } from "./services/Menu";
 
+window.app = {}
+app.store = Store;
+
+window.addEventListener("DOMContentLoaded", async () => {
+    loadData();
 });
