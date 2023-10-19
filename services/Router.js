36f04d22908f9ae280af9236a022892e063cd404
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-19 12:55:48 -0400

    added getAttribute

--- services/Router.js
+++ services/Router.js
@@ -3,6 +3,8 @@ const Router = {
         document.querySelectorAll("a.navlink").forEach(a => {
             a.addEventListener("click", event => {
                 event.preventDefault();
+                const url = event.target.getAttribute("href");
+                Router.go(url);
             });
         })
     },
