0c3cdf724014806ba755ddd5afe0357e4d18e737
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-20 20:51:38 -0400

    update index

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -2,13 +2,15 @@ export class NewPage extends HTMLElement {
     constructor() {
         super();
 
+        this.root = this.attachShadow({ mode: 'open'});
+
     }
 
     // When the component is attached to the DOM
     connectedCallback() {
         const template = document.getElementById("menu-page-template");
         const content = template.content.cloneNode(true);
-        this.appendChild(content);
+        this.root.appendChild(content);
     }
 }
 customElements.define("menu-page", MenuPage);
