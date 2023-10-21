4f5516decf92687ab53e4ac80caac5fd16af7d46
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-20 20:33:20 -0400

    details

--- components/DetailsPage.js
+++ components/DetailsPage.js
@@ -1,5 +1,14 @@
-class NewPage extends HTMLElement {
+export class NewPage extends HTMLElement {
     constructor() {
         super();
+
+    }
+
+    // When the component is attached to the DOM
+    connectedCallback() {
+        const template = document.getElementById("details-page-template");
+        const content = template.content.cloneNode(true);
+        this.appendChild(content);
     }
 }
+customElements.define("details-page", DetailsPage);
