6fd860795542835495647e675e41943157b2dc4f
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-20 21:40:22 -0400

    custom element

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -35,6 +35,12 @@ export class NewPage extends HTMLElement {
                     </ul>
                 `;
                 this.root.querySelector("menu").appendChild(liCategory);
+
+                category.products.forEach(product => {
+                    const item = document.createElement("product-item");
+                    item.dataset.product = JSON.stringify(product);
+                    liCategory.querySelector("ul").appendChild(item);
+                });
             }
         } else {
             this.root.querySelector("menu").innerHTML = "loading...";
